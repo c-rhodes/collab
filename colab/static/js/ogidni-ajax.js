747ae02f073f2531de2511264127748718e2769d
Steven Gergus <Gergy@Stevens-MacBook-Pro.local>
2014-02-14 19:01:43 +0000

    Ajax

--- colab/static/js/ogidni-ajax.js
+++ colab/static/js/ogidni-ajax.js
@@ -15,7 +15,9 @@ function replyvote(DIR_, TYPE_, id) {
 	switch (Number(TYPE_)){
 		case TYPE.S:
 			$.ajax({
-				url: "/odigni/vote_story/?dir="+Number(DIR_)+"&id="+id,
+				type: "GET",
+				url: "/ogidni/vote_story",
+				data:"dir="+Number(DIR_)+"&story_id="+id,
 				statusCode: {
 					204: function() {
 						alert("No content");
@@ -34,7 +36,9 @@ function replyvote(DIR_, TYPE_, id) {
 			break;
 		case TYPE.R:
 			$.ajax({
-				url: "/odigni/vote_reply?dir="+Number(DIR_)+"&id="+id,
+				type: "GET",
+				url: "/ogidni/vote_reply",
+				data:"dir="+Number(DIR_)+"&reply_id="+id,
 				statusCode: {
 					204: function() {
 						alert("No content");
