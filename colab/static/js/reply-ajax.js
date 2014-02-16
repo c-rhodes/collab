97d6dbcec01ece0b9603736b8d11cc1d75f4f525
Steven Gergus <Gergy@Stevens-MacBook-Pro.local>
2014-02-16 20:29:42 +0000

    Finalised visual changes to WYSIHTML5
    Paved the way to implement ajax

--- colab/static/js/reply-ajax.js
+++ colab/static/js/reply-ajax.js
@@ -1,4 +1,12 @@
-$(".textarea").wysihtml5();
+$(".textarea").wysihtml5({
+    "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+    "emphasis": true, //Italics, bold, etc. Default true
+    "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+    "html": false, //Button which allows you to edit the generated HTML. Default false
+    "link": false, //Button to insert a link. Default true
+    "image": false, //Button to insert an image. Default true,
+    "color": true //Button to change color of font  
+});
 var mousePos = {top:100, left:100};
 var currentReplyID = {};
 var TYPE = {
