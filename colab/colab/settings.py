ad057cd2a778e7df293abf144c6836591220921e
Cullen Rhodes <rhodes.cullen@yahoo.co.uk>
2014-03-09 20:58:02 +0000

    added django debug toolbar

--- colab/colab/settings.py
+++ colab/colab/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'ogidni',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
